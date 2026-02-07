c6eb865b34f4861fc50bc10f5f4606fa689dc62d
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-07 14:45:23 +0530

    installing mysql too

--- roboshop.sh
+++ roboshop.sh
@@ -2,8 +2,58 @@
 
 SG_ID="sg-0bee7a3b92682ee26"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z09128621T3RXE44PBW3M"
+DOMAIN_NAME="hamsa.sbs"
 
 for instance in $@
 do
-    aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].PrivateIpAddress' --output text
+   INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' --output text )
+
+    if [ $instance == 'frontend']; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" #HAMSA.SBS
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" #MONGODB.HAMSA.SBS
+    fi
+
+    echo "IP address is: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+    echo "record updated for $instance"
 done
