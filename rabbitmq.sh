eb6162bb7f63fd8a971e3569eaa965c50cb437f8
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-07 19:39:07 +0530

    installing mysql too

---
+++ rabbitmq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.hamsa.sbs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then  
+    echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE
+    exit 1
+    else
+        echo -e "$2...$R SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Added rabbitmq repo"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "installed rabbitmq server"
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created user and given permissions"
